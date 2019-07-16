aca2b9cd64c3e4d6e8276a17eb09c5f19f2966ce
CRAFIS <crafisbandicoot@gmail.com>
2019-07-16 21:14:45 +0900

    Update

--- FavoriteList.py
+++ FavoriteList.py
@@ -2,10 +2,11 @@ import MyTwitter, sys
 
 def execute(user_name, list_name):
     twitter, user_id = MyTwitter.login(user_name)
-    id_list = [tweet["id_str"] for tweet in MyTwitter.get_tweet_list(twitter, user_id, 200)]
-    user_list = []
+    tweets = MyTwitter.get_tweet_list(twitter, user_id, 200)
+    id_list = [tweet["id_str"] for tweet in tweets if not MyTwitter.is_timeover(tweet['created_at'], 2)]
     friend_list = MyTwitter.get_following_id(twitter, user_id)
-    for tweet_id in id_list[:10]:
+    user_list = []
+    for tweet_id in id_list:
         user_list.extend(MyTwitter.get_fav_user_id_list(tweet_id, [user_id]))
     user_list = [user for user in list(set(user_list)) if user in friend_list]
     list_id = MyTwitter.get_list_id(list_name)

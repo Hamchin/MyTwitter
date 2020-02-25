03aa4ad63b73ca9823d4e811dbfa22588708dcb4
KeyTey <k.terada.1211@gmail.com>
2020-02-26 02:35:10 +0900

    Update

--- LikedList.py
+++ LikedList.py
@@ -3,7 +3,7 @@ import MyTwitter, sys, requests, json
 # 通知取得
 def get_notices(size = 10):
     params = {'size': size}
-    res = requests.get("https://notice-database.herokuapp.com/api/notices", params = params)
+    res = requests.get("https://notice-database.herokuapp.com/notices", params = params)
     notices = json.loads(res.text)
     return notices
 
@@ -15,31 +15,30 @@ def get_list_members(twitter, list_name):
     return members
 
 # リストへユーザー追加
-def add_users(twitter, list_name, target_names, member_names, trim_names):
+def add_users(twitter, list_name, target_ids, member_ids, trim_ids):
     list_id = MyTwitter.get_list_id(list_name)
-    for target_name in target_names:
-        if target_name in trim_names: continue
-        if target_name in member_names: continue
-        MyTwitter.add_user(twitter, list_id, screen_name = target_name)
+    for target_id in target_ids:
+        if target_id in trim_ids: continue
+        if target_id in member_ids: continue
+        MyTwitter.add_user(twitter, list_id, user_id = target_id)
 
 # リストからユーザー削除
-def delete_users(twitter, list_name, target_names, member_names, trim_names):
+def delete_users(twitter, list_name, target_ids, member_ids, trim_ids):
     list_id = MyTwitter.get_list_id(list_name)
-    for member_name in member_names:
-        if member_name not in target_names or member_name in trim_names:
-            MyTwitter.delete_user(twitter, list_id, screen_name = member_name)
+    for member_id in member_ids:
+        if member_id not in target_ids or member_id in trim_ids:
+            MyTwitter.delete_user(twitter, list_id, user_id = member_id)
 
 # いいねリスト更新
 def execute(list_name, trim_list_name = ''):
     twitter, self_id = MyTwitter.login()
-    self_user = MyTwitter.get_user(twitter, user_id = self_id)
     notices = get_notices(size = 100)
-    notices = [notice for notice in notices if notice['receive_user'] == self_user['screen_name']]
-    member_names = [user['screen_name'] for user in get_list_members(twitter, list_name)]
-    trim_names = [user['screen_name'] for user in get_list_members(twitter, trim_list_name)]
-    notice_senders = list(set([notice['send_user'] for notice in notices]))
-    add_users(twitter, list_name, notice_senders, member_names, trim_names)
-    delete_users(twitter, list_name, notice_senders, member_names, trim_names)
+    notices = [notice for notice in notices if notice['receiver_id'] == self_id]
+    member_ids = [user['id_str'] for user in get_list_members(twitter, list_name)]
+    trim_ids = [user['id_str'] for user in get_list_members(twitter, trim_list_name)]
+    sender_ids = list(set([notice['sender_id'] for notice in notices]))
+    add_users(twitter, list_name, sender_ids, member_ids, trim_ids)
+    delete_users(twitter, list_name, sender_ids, member_ids, trim_ids)
 
 if __name__ == '__main__':
     if len(sys.argv) == 3:

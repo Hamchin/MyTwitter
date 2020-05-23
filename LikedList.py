07c98be30f7b27942c72176e78f7f08ded57d2b6
KeyTey <k.terada.1211@gmail.com>
2020-05-23 17:58:00 +0900

    Update

--- LikedList.py
+++ LikedList.py
@@ -1,54 +1,82 @@
 import MyTwitter, NoticeDB, sys, requests, json
 
-# 通知取得
-def get_notices(size = 10):
-    url = NoticeDB.NOTICE_API['ENDPOINT'] + NoticeDB.NOTICE_API['GET_NOTICES_URI']
-    params = {'size': size}
-    res = requests.get(url, params = params)
-    notices = json.loads(res.text)
-    return notices
+class List():
+    name = ''
+    id = ''
+    member_ids = []
+    trim_friends = False # フォローしているユーザーを除外するかどうか
+    trim_not_friends = False # フォローしていないユーザーを除外するかどうか
 
-# リストネームからリストメンバー取得
-def get_list_members(twitter, list_name):
-    if list_name == '': return []
-    list_id = MyTwitter.get_list_id(list_name)
-    members = MyTwitter.get_list_members(twitter, list_id)
-    return members
+    def __init__(self, name, trim_friends = False, trim_not_friends = False):
+        self.name = name
+        self.id = MyTwitter.get_list_id(self.name)
+        self.trim_friends = trim_friends
+        self.trim_not_friends = trim_not_friends
+        twitter, user_id = MyTwitter.login()
+        members = MyTwitter.get_list_members(twitter, self.id)
+        self.member_ids = [member['id_str'] for member in members]
 
-# リストへユーザー追加
-def add_users(twitter, list_name, target_ids, member_ids, trim_ids):
-    list_id = MyTwitter.get_list_id(list_name)
-    for target_id in target_ids:
-        if target_id in trim_ids: continue
-        if target_id in member_ids: continue
-        target = MyTwitter.get_user(twitter, user_id = target_id)
-        if target is None: continue
-        if not target['following']: continue
-        MyTwitter.add_user(twitter, list_id, user_id = target_id)
+class ListUpdater():
+    twitter = None
+    user_id = ''
+    notices = []
+    sender_ids = []
+    friend_ids = []
+    list_instances = []
+    trim_list_instance = None
 
-# リストからユーザー削除
-def delete_users(twitter, list_name, target_ids, member_ids, trim_ids):
-    list_id = MyTwitter.get_list_id(list_name)
-    for member_id in member_ids:
-        if member_id not in target_ids or member_id in trim_ids:
-            MyTwitter.delete_user(twitter, list_id, user_id = member_id)
+    def __init__(self, list_instances, trim_list_instance):
+        self.twitter, self.user_id = MyTwitter.login()
+        self.list_instances = list_instances
+        self.trim_list_instance = trim_list_instance
+        self.notices = self.get_notices(100)
+        self.sender_ids = list(set([notice['sender_id'] for notice in self.notices]))
+        self.friend_ids = MyTwitter.get_friend_ids(self.twitter, self.user_id)
 
-# いいねリスト更新
-def execute(list_name, trim_list_name = ''):
-    twitter, self_id = MyTwitter.login()
-    notices = get_notices(size = 100)
-    notices = [notice for notice in notices if notice['receiver_id'] == self_id]
-    member_ids = [user['id_str'] for user in get_list_members(twitter, list_name)]
-    trim_ids = [user['id_str'] for user in get_list_members(twitter, trim_list_name)]
-    sender_ids = list(set([notice['sender_id'] for notice in notices]))
-    add_users(twitter, list_name, sender_ids, member_ids, trim_ids)
-    delete_users(twitter, list_name, sender_ids, member_ids, trim_ids)
+    # 通知取得
+    def get_notices(self, size):
+        url = NoticeDB.NOTICE_API['ENDPOINT'] + NoticeDB.NOTICE_API['GET_NOTICES_URI']
+        params = {'size': size}
+        res = requests.get(url, params = params)
+        notices = json.loads(res.text)
+        notices = [notice for notice in notices if notice['receiver_id'] == self.user_id]
+        return notices
+
+    # リストへユーザー追加
+    def add_users(self, list_instance, target_ids):
+        for target_id in target_ids:
+            if target_id in list_instance.member_ids: continue
+            MyTwitter.add_user(self.twitter, list_instance.id, user_id = target_id)
+
+    # リストからユーザー削除
+    def delete_users(self, list_instance, target_ids):
+        for member_id in list_instance.member_ids:
+            if member_id in target_ids: continue
+            MyTwitter.delete_user(self.twitter, list_instance.id, user_id = member_id)
+
+    # リスト更新
+    def update(self):
+        for list_instance in list_instances:
+            target_ids = self.sender_ids
+            if self.trim_list_instance:
+                trim_ids = self.trim_list_instance.member_ids
+                target_ids = [id for id in target_ids if id not in trim_ids]
+            if list_instance.trim_friends:
+                target_ids = [id for id in target_ids if id not in self.friend_ids]
+            if list_instance.trim_not_friends:
+                target_ids = [id for id in target_ids if id in self.friend_ids]
+            self.add_users(list_instance, target_ids)
+            self.delete_users(list_instance, target_ids)
 
 if __name__ == '__main__':
-    if len(sys.argv) == 3:
-        execute(sys.argv[1], sys.argv[2])
-    elif len(sys.argv) == 2:
-        execute(sys.argv[1])
-    else:
-        print("Usage: python3 {0} [LIST_NAME] (EXCLUDED_LIST_NAME)".format(sys.argv[0]))
+    expected_args = ['[FIRST_LIST_NAME]', '[SECOND_LIST_NAME]', '[EXCLUDED_LIST_NAME]']
+    if len(sys.argv) != len(expected_args) + 1:
+        print(f"Usage: python3 {sys.argv[0]} {' '.join(expected_args)}")
         sys.exit()
+    list_instances = [
+        List(sys.argv[1], trim_not_friends = True),
+        List(sys.argv[2], trim_friends = True)
+    ]
+    trim_list_instance = List(sys.argv[3])
+    updater = ListUpdater(list_instances, trim_list_instance)
+    updater.update()

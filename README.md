9523a78d05a2dbd7717469636080dc2c2bf28433
Hamchin <k.terada.1211@gmail.com>
2022-01-15 06:12:00 +0900

    Update

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 ## crontab
 
 ```sh
-0 * * * * cd $HOME/MyTwitter && ./venv/bin/python main.py check_followers
-0 * * * * cd $HOME/MyTwitter && ./venv/bin/python main.py customize
+0 * * * * cd $HOME/MyTwitter && ./venv/bin/python main.py --mode check_followers
+0 * * * * cd $HOME/MyTwitter && ./venv/bin/python main.py --mode customize
 ```
 
 ## Lambda Function
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import argparse
 import json
-import sys
 from src import check_followers
 from src import check_notices
 from src import customize
@@ -8,6 +8,16 @@ from src import recommend
 from src.twitter import Twitter
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mode', required = True, choices = [
+        'check_followers',
+        'check_notices',
+        'customize',
+        'delete_tweets',
+        'recommend'
+    ])
+    args = parser.parse_args()
+
     config = json.load(open('config/config.json', 'r'))
 
     twitter = Twitter(
@@ -17,14 +27,13 @@ if __name__ == '__main__':
         config['access_secret']
     )
 
-    mode = sys.argv[1] if len(sys.argv) >= 2 else ''
-    if mode == 'check_followers':
+    if args.mode == 'check_followers':
         check_followers.main(twitter)
-    elif mode == 'check_notices':
+    elif args.mode == 'check_notices':
         check_notices.main(twitter, config['notice_api_url'])
-    elif mode == 'customize':
+    elif args.mode == 'customize':
         customize.main(twitter, config['notice_api_url'], **config['customize'])
-    elif mode == 'delete_tweets':
+    elif args.mode == 'delete_tweets':
         delete_tweets.main(twitter)
-    elif mode == 'recommend':
+    elif args.mode == 'recommend':
         recommend.main(twitter)
